57a24fb99d1ef431692fffb8c7d3add38a777b0f
rdianach <xdianamarx@gmail.com>
2022-09-01 06:08:20 +0700

    belajar_golang

---
+++ 01_dasar/06_tipe_data_string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Amardian")
+	fmt.Println("Achmad")
+	fmt.Println("===")
+
+	fmt.Println(len("Amardian")) // menghitung panjang string
+	fmt.Println("Achmad"[2])     // byte 104
+
+}
